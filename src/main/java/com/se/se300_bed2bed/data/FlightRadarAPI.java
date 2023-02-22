8a2cfbe3491d510c4f657af9234642b17febb983
patrick4k <pmkennedy4@gmail.com>
2023-02-22 16:05:00 -0500

    implement methods

--- src/main/java/com/se/se300_bed2bed/data/FlightRadarAPI.java
+++ src/main/java/com/se/se300_bed2bed/data/FlightRadarAPI.java
@@ -1,5 +1,16 @@
 package com.se.se300_bed2bed.data;
 
+import java.util.HashMap;
+
 public class FlightRadarAPI extends APIDatabase {
 
+    @Override
+    protected boolean CONNECT() {
+        return false;
+    }
+
+    @Override
+    protected HashMap<String, String> GET() {
+        return null;
+    }
 }
