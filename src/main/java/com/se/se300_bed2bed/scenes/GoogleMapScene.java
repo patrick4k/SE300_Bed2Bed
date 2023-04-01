b32b31bea720cfef98b1e18e5345c1397e8e99c7
andreagamble <abgamble00@gmail.com>
2023-04-01 16:27:24 -0400

    LoginScene.fxml/LoginScene Java Class.
    Links to CreateAccountScene.java

--- src/main/java/com/se/se300_bed2bed/scenes/GoogleMapScene.java
+++ src/main/java/com/se/se300_bed2bed/scenes/GoogleMapScene.java
@@ -1,9 +1,13 @@
 package com.se.se300_bed2bed.scenes;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.web.WebView;
 
-public class GoogleMapScene extends FXMLController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GoogleMapScene extends FXMLController implements Initializable {
     @Override
     protected String fxmlName() {
         return "GoogleMapScene.fxml";
@@ -12,4 +16,12 @@ public class GoogleMapScene extends FXMLController{
     @FXML
     private WebView mapView;
 
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+    public void loadPage() {
+       // engine.load("MapDisplay.html");
+    }
 }
