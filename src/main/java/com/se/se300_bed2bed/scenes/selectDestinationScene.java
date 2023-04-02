3a901feef4af37533783c057c9304bafe925f08d
andreagamble <abgamble00@gmail.com>
2023-04-02 14:06:36 -0400

    LoginScene.fxml/LoginScene Java Class.
    Links to CreateAccountScene.java

--- src/main/java/com/se/se300_bed2bed/scenes/selectDestinationScene.java
+++ src/main/java/com/se/se300_bed2bed/scenes/selectDestinationScene.java
@@ -2,8 +2,15 @@ package com.se.se300_bed2bed.scenes;
 
 import com.se.se300_bed2bed.Bed2BedApp;
 import com.se.se300_bed2bed.util.LLAPosition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class selectDestinationScene extends FXMLController{
     @Override
@@ -15,6 +22,9 @@ public class selectDestinationScene extends FXMLController{
     @FXML
     private TextField destinationText;
 
+    protected WebView webView;
+
+
     LLAPosition getCoords(double latitude, double longitude) {
 
 
@@ -23,6 +33,19 @@ public class selectDestinationScene extends FXMLController{
 
     public void goToGoogleMapScene() {
         Bed2BedApp.TryGoTo(GoogleMapScene.class);
+    }
+
+    public void autoComplete(ActionEvent event) {
+        String from = originText.getText();
+        String to = destinationText.getText();
+
+        this.webView = new WebView();
+        URL url = this.getClass().getResource("AutoComplete.html");
+        assert url != null;
+        webView.getEngine().load(url.toString());
+
+        webView.getEngine().executeScript(from + to + "autoComplete();");
+
     }
 
 }
