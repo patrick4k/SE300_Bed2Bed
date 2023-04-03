3da63628c4a2f7caf24b6e51f0cbb0e66ad84917
gBurte <123216832+gBurte@users.noreply.github.com>
2023-04-03 16:44:22 -0400

    Initial Commit

---
+++ src/main/java/com/se/se300_bed2bed/scenes/StartingLoc.java
@@ -0,0 +1,8 @@
+package com.se.se300_bed2bed.scenes;
+
+public class StartingLoc extends FXMLController {
+    @Override
+    protected String fxmlName() {
+        return "StartingLoc.fxml";
+    }
+}
